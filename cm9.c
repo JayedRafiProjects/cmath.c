f18c358ace3e836e11c1f08c9c0cd2fc38be9101
Jayed Rafi <jayedrafi.me@gmail.com>
2021-10-12 21:31:24 -0500

    Add files via upload

---
+++ cm9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+/*
+* x^n-y^n
+*/
+int pow_n_min(int x, int y, int n)
+{
+    return pow(x,n)-pow(y,n);
+}
+
+int main(void)
+{
+    int a = 2;
+    int b = 5;
+    int p = 3;
+    int result = pow_n_min(a,b,p);
+    printf("Result: %d\n",result);
+    return 0;
+}
