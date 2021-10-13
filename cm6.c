63a2057f669776e115c11c48971bf239fd8a9585
Jayed Rafi <jayedrafi.me@gmail.com>
2021-10-12 21:18:23 -0500

    Update cm6.c

--- cm6.c
+++ cm6.c
@@ -10,11 +10,12 @@ double radical(x,n)
     answer = pow(x,1.0/n);
     return answer;
 }
-int main()
+int main(void)
 {
     //This should prints 2 since the squareroot of 4 is 2
     printf("%f\n",radical(4,2));
 
     //This should prints 3 since the cubic root of 27 is 3
     printf("%f",radical(27,3));
+    return 0;
 }
