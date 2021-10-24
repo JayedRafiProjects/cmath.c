2436e993b06e3a30151892ac924489d88feea9a4
Jayed Rafi <jayedrafi@outlook.com>
2021-10-24 14:38:31 -0500

    Create cmg1.c

---
+++ geometry/cmg1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+/****************************************
+* Project CMath
+*
+* EQUATION: area of hexagon
+* BRANCH: Geometry
+* VARIABLE DESCRIPTION: s: side of hexagon
+****************************************/
+double area_hexagon(double s){
+    return (3*sqrt(3)*pow(s,2))/2;
+}
+
+int main(void){
+    double side_of_hexagon = 12;
+    double result = area_hexagon(side_of_hexagon);
+    printf("Area of the hexagon: %f\n",result);
+    
+    return 0;
+}
