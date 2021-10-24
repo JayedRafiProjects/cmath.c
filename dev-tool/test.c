42a2a483893a4dd2c9a8f73df9434fb5572082b8
Jayed Rafi <jayedrafi@outlook.com>
2021-10-24 01:46:51 -0500

    Update test.c
    Programming style demonstration

--- dev-tool/test.c
+++ dev-tool/test.c
@@ -1,5 +1,18 @@
-//read instruction.txt
 #inclide<stdio.h>
+
+/****************************************
+* Project CMath
+*
+* Equation: 
+* Math branch: Geometry
+* Variable description: 
+*
+****************************************/
+double your_function(double x, double y, double z)
+{
+  return x+y+z;
+}
+
 
 int main(void)
 {
