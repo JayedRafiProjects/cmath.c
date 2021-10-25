da35bb470a3f8e33c0771699be861934ed2d7c72
Jayed Rafi <jayedrafi@outlook.com>
2021-10-25 11:14:06 -0500

    Create cmg2.c

---
+++ geometry/cmg2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+/****************************************
+* Project CMath
+*
+* EQUATION: area of a regular hexagon
+* BRANCH: Geometry
+* VARIABLE DESCRIPTION: p: perimeter, a: apothem
+****************************************/
+double area_hexagon(double p, double a){
+    return (p*a)/2;
+}
+
+int main(void){
+    double perimeter_of_hexagon = 60;
+    double apothem_of_hexagon = 5*sqrt(3);
+    double result = area_hexagon(perimeter_of_hexagon,apothem_of_hexagon);
+    printf("Area of the hexagon: %f\n",result);
+    
+    return 0;
+}
