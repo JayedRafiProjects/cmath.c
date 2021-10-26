3a9bd2cf48dc9626c697e52b0d7a3bed3d92a56b
Jayed Rafi <jayedrafi@outlook.com>
2021-10-26 12:52:38 -0500

    Update cmc1.c

--- combinatorics/cmc1.c
+++ combinatorics/cmc1.c
@@ -1,1 +1,26 @@
-c
+#include<stdio.h>
+
+/****************************************
+* Project CMath
+*
+* EQUATION: n! factorial
+* BRANCH: combinatronics
+* VARIABLE DESCRIPTION: result: it always should be a constant value 1
+                        number: n
+****************************************/
+int nfactorial(int result, int number){
+    if(number==0)
+        return result;
+    else{
+        result*=number;
+        return nfactorial(result,number-1);
+    }
+}
+
+int main(void){
+    int n = 10;
+    int result = nfactorial(1,n);
+    printf("nFactorial is: %d\n",result);
+    
+    return 0;
+}
