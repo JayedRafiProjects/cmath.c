53b150853ab4f03317766814ce021fc5539ea9b6
Jayed Rafi <jayedrafi@outlook.com>
2021-10-28 13:21:52 -0500

    Update standard.c

--- dev-tool/standard.c
+++ dev-tool/standard.c
@@ -7,7 +7,7 @@
 * BRANCH: Arithmatic
 * VARIABLE DESCRIPTION: numbers[] is a array of numaric values
 ****************************************/
-double sum(double numbers[],int size){
+double sum_n(double numbers[],int size){
   double sum = 0;
   for(int i=0; i<size; i++)
   {
@@ -19,7 +19,7 @@ double sum(double numbers[],int size){
 
 int main(void){
   double numbers[] = {0,1,2,3,4,5,6,7,8,9};
-  double result = sum(numbers,10);
+  double result = sum_n(numbers,10);
   printf("sum: %d\n",result);
   
   return 0;
