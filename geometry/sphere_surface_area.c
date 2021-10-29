af8f69f6849d653f9db3a4916b3895d12b80eca8
Jayed Rafi <jayedrafi@outlook.com>
2021-10-29 10:46:00 -0500

    Update sphere_surface_area.c

--- geometry/sphere_surface_area.c
+++ geometry/sphere_surface_area.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <math.h>
-/*
- * Project CMath
- * Equation:surface area of a sphere(s.a= 4*pi*radius^2)
- * Branch: geometry
- * */
-float sphere_surface_area(float radius){
 
+/****************************************
+ * Project CMath
+ *
+ * Equation:surface area of a sphere(s.a= 4*pi*radius^2)
+ * BRANCH: Geometry
+ * VARIABLE DESCRIPTION: radius
+****************************************/
+float sphere_surface_area(float radius){
     return 4*M_PI*radius*radius;
 }
 
 int main(void){
-
-    printf("%f",sphere_surface_area(2));
+    printf("Surface area: %f\n",sphere_surface_area(2));
     return 0;
 }
