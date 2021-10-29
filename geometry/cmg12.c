4a7e8958bb35817d569529c422ddf8dfa8b5d1ca
Jayed Rafi <jayedrafi@outlook.com>
2021-10-29 10:21:56 -0500

    Update and rename cone_lateral_area.c to cmg12.c

---
+++ geometry/cmg12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+/****************************************
+ * Project CMath
+ *
+ * Equation: lateral area of a cone (l.a = pi*radius*length)
+ * BRANCH: Geometry
+ * VARIABLE DESCRIPTION: radius, length
+****************************************/
+float cone_lateral_area(float radius, float length){
+
+    return M_PI*radius*length;
+}
+
+int main(void){
+
+    printf("Area: %f\n",cone_lateral_area(2,4));
+    return 0;
+}
