47355e3697d8f506592814f5daefbb84277cfd96
Jayed Rafi <jayedrafi@outlook.com>
2021-10-29 10:33:09 -0500

    Update cylinder_surface_area.c

--- geometry/cylinder_surface_area.c
+++ geometry/cylinder_surface_area.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <math.h>
-/*
- * Project CMath
- * Equation:surface area of a cylinder(l.a= 2*pi*radius^2 +2*pi*radius*height)
- * Branch: geometry
- * */
-float cylinder_surface_area(float radius, float height){
 
+/****************************************
+ * Project CMath
+ *
+ * EQUATION: surface area of a cylinder(l.a= 2*pi*radius^2 +2*pi*radius*height)
+ * BRANCH: Geometry
+ * VARIABLE DESCRIPTION: radius, length
+****************************************/
+float cylinder_surface_area(float radius, float height){
     return 2*M_PI*radius*radius+2*M_PI*radius*height;
 }
 
 int main(void){
-
     printf("%f",cylinder_surface_area(2,4));
     return 0;
 }
