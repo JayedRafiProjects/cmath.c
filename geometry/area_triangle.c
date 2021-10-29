e9e3b4f3001b4e0f8f2162f8d084224f550de73a
Omar Taha <73119032+OmarEfat@users.noreply.github.com>
2021-10-29 13:01:23 -0500

    Add files via upload

---
+++ geometry/area_triangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void testing(double expected, double result)
+{
+    if(expected== result)
+        printf("PASSED: The expected is %f and the result is %f\n",expected,result);
+    else
+        printf("FAILED: The expected is %f and the result is %f\n",expected,result);
+}
+double area_triangle(double base , double height)
+{
+    return 0.5*base*height;
+}
+
+int main()
+{
+    testing(3,area_triangle(2,3));
+    testing(6,area_triangle(3,4));
+    testing(3000000,area_triangle(2000,3000));
+    testing(0.04375,area_triangle(0.35,0.25));
+
+}
+
+
+
