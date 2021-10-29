3e236e550b1dfbd6f9c01e54160ecc6d6f4e2db5
Jayed Rafi <jayedrafi@outlook.com>
2021-10-29 10:44:08 -0500

    Update and rename pyramid_volume.c to cmg21.c

--- geometry/cmg21.c
+++ geometry/cmg21.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <math.h>
-/*
+/****************************************
  * Project CMath
+ *
  * Equation:volume of a pyramid(v= (1/3)*(breadth*height))
- * Branch: geometry
- * */
+ * BRANCH: geometry
+ *VARIABLE DESCRIPTION: breadth, height
+****************************************/
 float pyramid_volume(float breadth, float height){
     return (breadth*height)/3;
 }
 
 int main(void){
-
-    printf("%f",pyramid_volume(2,8));
+    printf("Volume: %f\n",pyramid_volume(2,8));
     return 0;
 }
