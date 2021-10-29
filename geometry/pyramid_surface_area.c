21574ea15b76cdcd9171583ae1e3371ff5fc54f0
Jayed Rafi <jayedrafi@outlook.com>
2021-10-29 10:42:23 -0500

    Update pyramid_surface_area.c

--- geometry/pyramid_surface_area.c
+++ geometry/pyramid_surface_area.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-/*
+
+/****************************************
  * Project CMath
- * Equation:surface area of a pyramid(s.a= (1/2)*(length*perimeter)+area_of_base)
- * Branch: geometry
- * */
+ *
+ * EQUATION: surface area of a pyramid(s.a= (1/2)*(length*perimeter)+area_of_base)
+ * BRANCH: geometry
+ * VARIABLE DESCRIPTION: length, perimeter, area of the base
+****************************************/
 float pyramid_surface_area(float length, float perimeter,float area_of_base){
     return ((length*perimeter)/2)+area_of_base;
 }
 
 int main(void){
-
-    printf("%f",pyramid_surface_area(2,8,3));
+    printf("Surface area: %f\n",pyramid_surface_area(2,8,3));
     return 0;
 }
