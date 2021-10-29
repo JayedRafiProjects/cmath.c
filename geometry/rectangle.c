5fbbb67d5a40d0b78f9373fc357051d89b14036e
Omar Taha <73119032+OmarEfat@users.noreply.github.com>
2021-10-29 00:57:01 -0500

    Add files via upload

---
+++ geometry/rectangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+// Returning the are of rectangle by printing
+void testing(double expected, double result)
+{
+    if(expected== result)
+        printf("PASSED: The expected is %f and the result is %f\n",expected,result);
+    else
+        printf("FAILED: The expected is %f and the result is %f\n",expected,result);
+}
+double area_rectangle (double length , double width)
+{
+    return length*width;
+}
+
+int main()
+{
+    testing(6,area_rectangle(2,3));
+    testing(12,area_rectangle(3,4));
+    testing(8.84,area_rectangle(3.4,2.6));
+    testing(1,area_rectangle(1,1));
+
+}
+
+
+
