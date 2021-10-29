c18b99bf949db8621307fb5b39c5f9a78de5d35a
Jayed Rafi <jayedrafi@outlook.com>
2021-10-29 10:31:19 -0500

    Update and rename cylinder_lateral_area.c to cmg15.c

--- geometry/cmg15.c
+++ geometry/cmg15.c
@@ -1,11 +1,12 @@
-
 #include <stdio.h>
 #include <math.h>
-/*
+/****************************************
  * Project CMath
+ *
  * Equation:lateral area of a cylinder(l.a= 2*pi*radius*height)
- * Branch: geometry
- * */
+ * BRANCH: Geometry
+ * VARIABLE DESCRIPTION: radius, length
+****************************************/
 float cylinder_lateral_area(float radius, float height){
 
     return 2*M_PI*radius*height;
@@ -13,6 +14,6 @@ float cylinder_lateral_area(float radius, float height){
 
 int main(void){
 
-    printf("%f",cylinder_lateral_area(3,4));
+    printf("Area: %f\n",cylinder_lateral_area(3,4));
     return 0;
 }
