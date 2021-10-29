1e703a3cc15a40d9089aa8e0e6fb415dcec7873a
Jayed Rafi <jayedrafi@outlook.com>
2021-10-29 10:24:36 -0500

    Update and rename cone_surface_area.c to cmg13.c

---
+++ geometry/cmg13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+/****************************************
+ * Project CMath
+ *
+ * EQUATION: surface area of a cone(s.a = (2*pi*radius*(radius+height))
+ * BRANCH: Geometry
+ * VARIABLE DESCRIPTION: radius, length
+****************************************/
+float cone_surface_area(float radius, float length){
+
+    return 2*M_PI*radius*radius+M_PI*radius*length;
+}
+
+int main(void){
+
+    printf("Area: %f\n",cone_surface_area(2,4));
+    return 0;
+}
