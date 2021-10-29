75b839d1ac10b8a35328cc9f7c3387113693d078
Jayed Rafi <jayedrafi@outlook.com>
2021-10-29 10:35:41 -0500

    Update cylinder_volume.c

--- geometry/cylinder_volume.c
+++ geometry/cylinder_volume.c
@@ -1,18 +1,17 @@
-
 #include <stdio.h>
 #include <math.h>
-/*
- * Project CMath
- * Equation:volume area of a cylinder(v= pi*radius^2*height)
- * Branch: geometry
- * */
-float cylinder_volume(float radius, float height){
 
+/****************************************
+ * Project CMath
+ * EQUATION: volume area of a cylinder(v= pi*radius^2*height)
+ * BRANCH: Geometry
+ * VARIABLE DESCRIPTION: radius, length
+****************************************/
+float cylinder_volume(float radius, float height){
     return M_PI*radius*radius*height;
 }
 
 int main(void){
-
-    printf("%f",cylinder_volume(2,4));
+    printf("Volume: %f\n",cylinder_volume(2,4));
     return 0;
 }
