cb85735557e1b0cc801e98f24f45b0d5d501f1aa
Jayed Rafi <jayedrafi@outlook.com>
2021-10-30 07:26:48 -0500

    Update area_trapeziod.c

--- geometry/area_trapeziod.c
+++ geometry/area_trapeziod.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
-void testing(double expected, double result)
-{
+
+void testing(double expected, double result){
     if(expected== result)
         printf("PASSED: The expected is %f and the result is %f\n",expected,result);
     else
         printf("FAILED: The expected is %f and the result is %f\n",expected,result);
 }
-double area_trapeziod(double b1 , double b2 , double height)
-{
+
+/****************************************
+ * Project CMath
+ *
+ * EQUATION: area of trapeziod
+ * BRANCH: Geometry
+ * VARIABLE DESCRIPTION: base1, base2 and height
+****************************************/
+double area_trapeziod(double b1 , double b2 , double height){
     return 0.5*height*(b1+b2);
 }
 
-int main()
-{
+int main(void){
     testing(15,area_trapeziod(2,3,6));
     testing(35,area_trapeziod(3,4,10));
     testing(4500000,area_trapeziod(1000,2000,3000));
     testing(0.3125,area_trapeziod(1,0.25,0.5));
-
+    return 0;
 }
-
-
-
