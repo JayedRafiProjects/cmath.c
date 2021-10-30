cd8a2ed24bb35f2849e71708d6c8ae1d0bcbec83
Jayed Rafi <jayedrafi@outlook.com>
2021-10-30 07:24:35 -0500

    Update area_paralleogram.c

--- geometry/area_paralleogram.c
+++ geometry/area_paralleogram.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
-void testing(double expected, double result)
-{
+
+void testing(double expected, double result){
     if(expected== result)
         printf("PASSED: The expected is %f and the result is %f\n",expected,result);
     else
         printf("FAILED: The expected is %f and the result is %f\n",expected,result);
 }
-double area_paralleogram(double base , double height)
-{
+
+/****************************************
+ * Project CMath
+ *
+ * EQUATION: area of parallelogram
+ * BRANCH: Geometry
+ * VARIABLE DESCRIPTION: base and height
+****************************************/
+double area_parallelogram(double base , double height){
     return base*height;
 }
 
-int main()
-{
-    testing(6,area_paralleogram(2,3));
-    testing(12,area_paralleogram(3,4));
-    testing(6000000,area_paralleogram(2000,3000));
-    testing(0.0875,area_paralleogram(0.35,0.25));
-
+int main(void){
+    testing(6,area_parallelogram(2,3));
+    testing(12,area_parallelogram(3,4));
+    testing(6000000,area_parallelogram(2000,3000));
+    testing(0.0875,area_parallelogram(0.35,0.25));
+    return 0;
 }
-
-
-
