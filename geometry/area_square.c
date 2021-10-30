b336eb6005f7ac2067b03821183c330cef9f7b20
Jayed Rafi <jayedrafi@outlook.com>
2021-10-30 07:16:45 -0500

    Update area_square.c

--- geometry/area_square.c
+++ geometry/area_square.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
-void testing(double expected, double result)
-{
+
+void testing(double expected, double result){
     if(expected== result)
         printf("PASSED: The expected is %f and the result is %f\n",expected,result);
     else
         printf("FAILED: The expected is %f and the result is %f\n",expected,result);
 }
-double area_square(double side)
-{
+
+/****************************************
+* Project CMath
+*
+* EQUATION: area of square
+* BRANCH: Geometry
+* VARIABLE DESCRIPTION: side of square
+****************************************/
+double area_square(double side){
     return side*side;
 }
 
-int main()
-{
+int main(){
     testing(4,area_square(2));
     testing(9,area_square(3));
     testing(4000000,area_square(2000));
     testing(0.09,area_square(0.30));
-
 }
-
-
-
