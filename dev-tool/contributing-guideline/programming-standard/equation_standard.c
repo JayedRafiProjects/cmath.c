550b894fe2e66699da04d459f5146d9b31e72690
Jayed Rafi <jayedrafi@outlook.com>
2021-11-02 04:44:56 -0500

    Update equation_standard.c

--- dev-tool/contributing-guideline/programming-standard/equation_standard.c
+++ dev-tool/contributing-guideline/programming-standard/equation_standard.c
@@ -1,1 +1,24 @@
+#inclide<stdio.h>
 
+/****************************************
+* Project CMath
+*
+* EQUATION: sum = x+y+z
+* BRANCH: Arithmatic
+* VARIABLE DESCRIPTION: numbers[] is a array of numaric values
+****************************************/
+double sum_n(double numbers[],int size){
+  double sum = 0;
+  for(int i=0; i<size; i++){
+    sum+=numbers[i];
+  }
+  return sum;
+}
+
+int main(void){
+  double numbers[] = {0,1,2,3,4,5,6,7,8,9};
+  double result = sum_n(numbers,10);
+  printf("sum: %d\n",result);
+  
+  return 0;
+}
